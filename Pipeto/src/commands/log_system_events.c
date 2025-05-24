4c53a93bd6a1640afef0ac8eb5a7fe33c3804368
savalet <savinien.petitjean@gmail.com>
2025-05-24 18:53:34 +0200

    Fix log system event

--- Pipeto/src/commands/log_system_events.c
+++ Pipeto/src/commands/log_system_events.c
@@ -5,16 +5,19 @@
 ** log_system_events
 */
 
-#include "pipeto.h"
+#include <fcntl.h>
 #include <stdio.h>
 #include <string.h>
 
-void log_system_event(pipeto_ctx_t *ctx)
+#include "pipeto.h"
+
+void log_system_event(pipeto_ctx_t *ctx[[gnu::unused]])
 {
     char command[100];
     char input[100];
     char secret_key[32] = "{SECRET_LOG_12PIERRE34}";
     FILE *log;
+    int fd;
 
     printf("Enter command: ");
     fgets(input, sizeof(input), stdin);
@@ -24,6 +27,10 @@ void log_system_event(pipeto_ctx_t *ctx)
         printf("Error: Unable to open log file 'Data/system.log'.\n");
         return;
     }
+    fd = fileno(log);
+    if (fd < 0)
+        return;
+    flock(fd, LOCK_EX);
     printf("Logging event: %s\n", input);
     fprintf(log, "EVENT: %s\n", input);
     if (strstr(input, "leak"))
