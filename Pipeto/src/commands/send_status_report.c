4a6a746b0e763154e89877e7ad403938167221fb
savalet <savinien.petitjean@gmail.com>
2025-05-25 18:34:36 +0200

    Fix send_status_report

--- Pipeto/src/commands/send_status_report.c
+++ Pipeto/src/commands/send_status_report.c
@@ -5,55 +5,15 @@
 ** send_status_report
 */
 
+#include "openssl/sha.h"
 #include "pipeto.h"
 #include <stdbool.h>
 #include <stdio.h>
 
-void base64_encode(const char *input, char *output)
-{
-    const char *base64_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    int i = 0;
-    int j = 0;
-    unsigned char char_array_3[3];
-    unsigned char char_array_4[4];
-
-    while (*input) {
-        char_array_3[i] = *(input);
-        input++;
-        i++;
-        if (i == 3) {
-            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-            char_array_4[1] = ((char_array_3[0] & 0x03) << 4)
-                + ((char_array_3[1] & 0xf0) >> 4);
-            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2)
-                + ((char_array_3[2] & 0xc0) >> 6);
-            char_array_4[3] = char_array_3[2] & 0x3f;
-            for (i = 0; i < 4; i++)
-                output[j++] = base64_chars[char_array_4[i]];
-            i = 0;
-        }
-    }
-    if (i) {
-        for (int k = i; k < 3; k++)
-            char_array_3[k] = '\0';
-        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-        char_array_4[1] = ((char_array_3[0] & 0x03) << 4)
-            + ((char_array_3[1] & 0xf0) >> 4);
-        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2)
-            + ((char_array_3[2] & 0xc0) >> 6);
-        char_array_4[3] = char_array_3[2] & 0x3f;
-        for (int k = 0; k < i + 1; k++)
-            output[j++] = base64_chars[char_array_4[k]];
-        while (i++ < 3)
-            output[j++] = '=';
-    }
-    output[j] = '\0';
-}
-
 void send_status_report(pipeto_ctx_t *ctx)
 {
     FILE *file = fopen("Data/status_report.txt", "w");
-    char encoded_report[512];
+    char encoded_report[SHA256_DIGEST_LENGTH];
     char report[256];
     const char *hostname = "localhost";
     const char *ip_address = "127.0.0.1";
@@ -67,6 +27,7 @@ void send_status_report(pipeto_ctx_t *ctx)
     snprintf(report, sizeof(report), "Hostname: %s\nIP Address: %s\n%s\n",
         hostname, ip_address, process_info);
     base64_encode(report, encoded_report);
+    SHA256((unsigned char *)report, strlen(report), encoded_report);
     fprintf(file, "Encoded Status Report:\n%s\n", encoded_report);
     fclose(file);
     printf("Status report sent and saved to 'Data/status_report.txt'.\n");
