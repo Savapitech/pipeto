63d0857b93f639f20ac5250d9569433861df1bf4
savalet <savinien.petitjean@gmail.com>
2025-05-25 18:10:56 +0200

    Fix turbine_temperature

--- lib/src/turbine_temperature.c
+++ lib/src/turbine_temperature.c
@@ -9,27 +9,24 @@
 
 void turbine_temperature() {
     char input[128];
+    long long degress;
     
     printf("Enter the number of degrees you want to increase or decrease the turbine temperature : ");
     fgets(input, sizeof(input), stdin);
     input[strcspn(input, "\n")] = '\0';
-
-    int degrees = strtoll(input, NULL, 10);
-
+    degrees = strtoll(input, NULL, 10);
     if (degrees == INT_MAX - 1 || degrees == INT_MIN + 1) {
         printf("Turbine temperature is too unstable.\n");
         printf("{ERR0R TURBINE WILL EXPLODE}\n");
         exit(1);
     }
-
     printf("Turbine temperature is %d degrees.\n", 20);
-
     if (degrees < 0) {
-        printf("Turbine temperature is decreasing : %d\n", (20 + degrees));
+        printf("Turbine temperature is decreasing : %lld\n", (20 + degrees));
         return;
     }
-    else if (degrees > 0) {
-        printf("Turbine temperature is increasing : %d\n", (20 + degrees));
+    if (degrees > 0) {
+        printf("Turbine temperature is increasing : %lld\n", (20 + degrees));
         return;
     }
 }
